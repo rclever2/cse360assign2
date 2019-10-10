895a3c6db16644bc4560526cbc5a61f48394e3ef
russe <russe@DESKTOP-OEQEF82>
2019-10-09 21:24:26 -0700

    Initial Commit with Javadoc comments

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,56 @@
+/**
+ * @author Russell Cleverly
+ * Class ID: 70642
+ * Assignment #: 2
+ * 
+ */
+
+
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal ()
+	{
+	return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value:
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * @param value:
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	public String toString () 
+	{
+		return "";
+	}
+	
+	public void clear() 
+	{
+		
+	}
+}
